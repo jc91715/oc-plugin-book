78a5416ac78b8c49dc9f61fb229d747c7ccefd44
jc91715 <370234915@qq.com>
2019-06-12 17:21:06 +0800

    over fllow

--- assets/css/doc.js
+++ assets/css/doc.js
@@ -1,41 +1,47 @@
 jQuery(document).ready(function($){
-    var $doc = $('.docs-sidebar-toc'),
-        scrolling = false,
-        previousTop = 0,
-        currentTop = 0,
-        scrollDelta = 10,
-        scrollOffset = 150
 
-    $(window).on('scroll', function(){
-        if (!scrolling) {
-            scrolling = true
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
-            if (!window.requestAnimationFrame) {
-                setTimeout(autoFixedDoc, 250)
+    }else{
+        var $doc = $('.docs-sidebar-toc'),
+            scrolling = false,
+            previousTop = 0,
+            currentTop = 0,
+            scrollDelta = 10,
+            scrollOffset = 150
+
+        $(window).on('scroll', function(){
+            if (!scrolling) {
+                scrolling = true
+
+                if (!window.requestAnimationFrame) {
+                    setTimeout(autoFixedDoc, 250)
+                }
+                else {
+                    requestAnimationFrame(autoFixedDoc)
+                }
             }
-            else {
-                requestAnimationFrame(autoFixedDoc)
+        })
+
+        function autoFixedDoc() {
+            var currentTop = $(window).scrollTop()
+
+            // Scrolling up
+            if (previousTop - currentTop > scrollDelta) {
+                // $doc.removeClass('is-fixed')
+                $doc.addClass('fixed-top')
             }
+            else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+                // Scrolling down
+                $doc.addClass('is-fixed')
+                $doc.removeClass('fixed-top')
+            }
+            if (currentTop<50) {
+                $doc.removeClass('is-fixed')
+            }
+            previousTop = currentTop
+            scrolling = false
         }
-    })
-
-    function autoFixedDoc() {
-        var currentTop = $(window).scrollTop()
-
-        // Scrolling up
-        if (previousTop - currentTop > scrollDelta) {
-            // $doc.removeClass('is-fixed')
-            $doc.addClass('fixed-top')
-        }
-        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-            // Scrolling down
-            $doc.addClass('is-fixed')
-            $doc.removeClass('fixed-top')
-        }
-        if (currentTop<50) {
-            $doc.removeClass('is-fixed')
-        }
-        previousTop = currentTop
-        scrolling = false
     }
+
 });
