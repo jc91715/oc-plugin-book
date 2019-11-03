495e53102bb9386ff7f07f9d257799ac2dfb30cf
jcc <370234915@qq.com>
2019-11-03 11:32:55 +0800

    toc

--- models/Section.php
+++ models/Section.php
@@ -342,6 +342,7 @@ class Section extends Model
         if (is_null($chars)) {
             $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         }
+        $chars = str_shuffle($chars);
         mt_srand(10000000*(double)microtime());
         for ($i = 0, $str = '', $lc = strlen($chars)-1; $i < $len; $i++) {
             $str .= $chars[mt_rand(0, $lc)];
