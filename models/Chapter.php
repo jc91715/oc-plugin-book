d6172992a0ac44231412538988e4e06a8412432a
jcc <370234915@qq.com>
2019-11-03 12:24:06 +0800

    多次喧哗

--- models/Chapter.php
+++ models/Chapter.php
@@ -351,7 +351,7 @@ class Chapter extends Model
     {
         $val=str_replace('<code>','<code class="prettyprint">',str_replace('<pre>','<pre class="prettyprint">',$val));
 
-        return str_replace('<h6>', '<h6 id="aaa'.$this->getRandomString(8).'">', str_replace('<h5>', '<h5 id="aaa'.$this->getRandomString(8).'">', str_replace('<h4>', '<h4 id="aaa'.$this->getRandomString(8).'">', str_replace('<h3>', '<h3 id="aaa'.$this->getRandomString(8).'">', str_replace('<h2>', '<h2 id="aaa'.$this->getRandomString(8).'">', str_replace('<h1>', '<h1 id="aaa'.$this->getRandomString(8).'">', $val))))));
+        return str_replace('<h6>', '<h6 id="aaa'.$this->getRandomString(50).'">', str_replace('<h5>', '<h5 id="aaa'.$this->getRandomString(50).'">', str_replace('<h4>', '<h4 id="aaa'.$this->getRandomString(50).'">', str_replace('<h3>', '<h3 id="aaa'.$this->getRandomString(50).'">', str_replace('<h2>', '<h2 id="aaa'.$this->getRandomString(50).'">', str_replace('<h1>', '<h1 id="aaa'.$this->getRandomString(50).'">', $val))))));
 
     }
 
