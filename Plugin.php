016e5a6c5b7a107fe5287d5e99a9f0835be8d9be
jcc <370234915@qq.com>
2019-11-03 10:25:28 +0800

    添加搜索

--- Plugin.php
+++ Plugin.php
@@ -61,6 +61,85 @@ class Plugin extends PluginBase
         User::extend(function($model){
             $model->morphToMany ['chapters'] = [Chapter::class,'name'=>'userable'];
         });
+        \Event::listen('offline.sitesearch.query', function ($query) {
+
+            // Search your plugin's contents
+            $items = \Jc91715\Book\Models\Chapter::where('title', 'like', "%${query}%")
+                ->orWhere('content_html', 'like', "%${query}%")
+                ->get();
+
+            // Now build a results array
+            $results = $items->map(function ($item) use ($query) {
+
+                // If the query is found in the title, set a relevance of 2
+                $relevance = mb_stripos($item->title, $query) !== false ? 2 : 1;
+
+                // Optional: Add an age penalty to older results. This makes sure that
+                // newer results are listed first.
+                // if ($relevance > 1 && $item->published_at) {
+                //     $relevance -= $this->getAgePenalty($item->published_at->diffInDays(Carbon::now()));
+                // }
+
+                return [
+                    'title'     => $item->title,
+                    'text'      => $item->content_html,
+                    'url'       => '/docs/'.$item->doc_id.'/chapters/' . $item->id,
+                    'thumb'     => null, // Instance of System\Models\File
+                    'relevance' => $relevance, // higher relevance results in a higher
+                    // position in the results listing
+                    // 'meta' => 'data',       // optional, any other information you want
+                    // to associate with this result
+                    // 'model' => $item,       // optional, pass along the original model
+                ];
+            });
+
+
+
+
+            return [
+                'provider' => '--来自文档', // The badge to display for this result
+                'results'  => $results,
+            ];
+        });
+        \Event::listen('offline.sitesearch.query', function ($query) {
+
+            // Search your plugin's contents
+            $items = \Jc91715\Book\Models\Section::orWhere('content_html', 'like', "%${query}%")
+                ->get();
+
+            // Now build a results array
+            $results = $items->map(function ($item) use ($query) {
+
+                // If the query is found in the title, set a relevance of 2
+                $relevance = mb_stripos($item->title, $query) !== false ? 2 : 1;
+
+                // Optional: Add an age penalty to older results. This makes sure that
+                // newer results are listed first.
+                // if ($relevance > 1 && $item->published_at) {
+                //     $relevance -= $this->getAgePenalty($item->published_at->diffInDays(Carbon::now()));
+                // }
+
+                return [
+                    'title'     => $item->title,
+                    'text'      => $item->content_html,
+                    'url'       => '/docs/'.$item->doc_id.'/chapters/' . $item->chapter_id,
+                    'thumb'     => null ,// Instance of System\Models\File
+                    'relevance' => $relevance, // higher relevance results in a higher
+                    // position in the results listing
+                    // 'meta' => 'data',       // optional, any other information you want
+                    // to associate with this result
+                    // 'model' => $item,       // optional, pass along the original model
+                ];
+            });
+
+
+
+
+            return [
+                'provider' => '--来自文档', // The badge to display for this result
+                'results'  => $results,
+            ];
+        });
     }
 
     /**
